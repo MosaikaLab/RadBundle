82ff8298bac3159f1b4cb23d81e618f509cd054a
Andrea Pinti <dev@mosaika.it>
2018-01-12 16:44:59 +0100

    Command superclass revert

--- Command/BuildCommand.php
+++ Command/BuildCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Mosaika\RadBundle\Core\Generator\RadGenerator;
 use Mosaika\RadBundle\Event\BuildEvent;
 
-class BuildCommand extends \Symfony\Component\Console\Command\Command
+class BuildCommand extends ContainerAwareCommand
 {
 
     protected $types = array(
